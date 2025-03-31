3b7cfda85742b34991f736b81836f0e694669681
filip-byte <ohboytoby@gmail.com>
2025-03-31 14:52:47 +0100

    feat: add Artwork model class

---
+++ app/src/main/java/com/example/artgalleryapp/Artwork.java
@@ -0,0 +1,37 @@
+package com.example.artgalleryapp;
+
+public class Artwork {
+    private String id;
+    private String title;
+    private String imageUrl;
+
+    public Artwork(String id, String title, String imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
